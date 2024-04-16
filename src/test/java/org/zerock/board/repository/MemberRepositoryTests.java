db9e9fd793ba76a2f510a390084ad9582f471d7d
ACCOUNTING\petrak <tto4028@naver.com>
2024-04-16 15:04:47 +0900

    임시

--- src/test/java/org/zerock/board/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/MemberRepositoryTests.java
@@ -15,7 +15,7 @@ public class MemberRepositoryTests {
     private MemberRepository memberRepository;
 
     @Test
-    public void insertMembers() {
+    public void insertMembers() { // 깃 허브
 
         IntStream.rangeClosed(1, 100).forEach(i -> {
 
